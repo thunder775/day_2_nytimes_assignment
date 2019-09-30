273b6ab2a2fe128c603ab71ccdc746e8aec3082b
McLarenCollege <hello@mclarencollege.in>
2019-09-30 15:59:43 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+// News Articles
+//First:
+// Title: PM Modi seeks ideas for his IIT-Madras convocation speech
+// Description : In Chennai, PM Modi would also participate in the prize distribution ceremony of the Singapore-India Hackathon.
+// Image: https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2019/09/29/Pictures/pm-narendra-modi-smart-cities-mission_ee684f10-e2a8-11e9-93be-d8edb8f85faf.jpg
+
+// Second:
+// Title: Flooded Roads, Stranded Locals and Warning of More Rains
+// Description: Patna Nagar Nigam personnel, donning yellow raincoats, could be seen at various spots trying to unclog the manholes that have been choked by polythene and debris.
+// Image: https://images.news18.com/ibnlive/uploads/2019/09/Flood-Cartoon1.jpg
+
+// Third:
+// Title: Saudi crown prince denies ordering Jamal Khashoggi killing
+// Description: "Some think that I should know what 3 million people working for the Saudi government do daily", says Mohammed bin Salman
+// Image: https://www.thehindu.com/news/national/73kpsr/article26333229.ece/ALTERNATES/FREE_460/TH22KRASALMAN
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('New York Times'), backgroundColor: Colors.white,),
+        body: Container(),
+      ),
+    )
+  );
+}
